c779ab46f708a5f7220b161d408bf1ce99f4259a
Lucane99 <lucaneryr123@gmail.com>
2023-05-23 15:13:29 +0545

    changes

---
+++ controllers/productController.js
@@ -0,0 +1,183 @@
+const Product = require('../model/Product');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+
+module.exports.getAllProducts = async (req, res) => {
+
+  try {
+    const data = await Product.find();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+  }
+
+}
+
+
+module.exports.getProductById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'please provide valid id'
+      });
+    } else {
+      const data = await Product.findById({ _id: id });
+      return res.status(200).json(data);
+    }
+
+  } catch (err) {
+
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+  }
+
+}
+
+
+
+module.exports.productAdd = async (req, res) => {
+  const imagePath = req.imagePath;
+
+  const {
+    product_name,
+    product_detail,
+    product_price,
+    brand,
+    category,
+    countInStock
+  } = req.body;
+  try {
+
+    await Product.create({
+      product_name,
+      product_detail,
+      product_price,
+      brand,
+      category,
+      countInStock,
+      product_image: imagePath
+    });
+
+    return res.status(201).json({
+      status: 'success',
+      message: 'successfully created'
+    });
+
+  } catch (err) {
+    fs.unlink(`.${imagePath}`, (err) => {
+      console.log(err);
+    })
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+
+  }
+
+
+}
+
+module.exports.productUpdate = async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  const {
+    product_name,
+    product_detail,
+    product_price,
+    brand,
+    category,
+    countInStock
+  } = req.body;
+  try {
+
+    if (req.newImagePath) {
+      console.log('hello');
+      await Product.findByIdAndUpdate({ _id: id }, {
+        product_name,
+        product_detail,
+        product_price,
+        brand,
+        category,
+        countInStock,
+        product_image: req.newImagePath
+      });
+
+    } else {
+      await Product.findByIdAndUpdate({ _id: id }, {
+        product_name,
+        product_detail,
+        product_price,
+        brand,
+        category,
+        countInStock,
+      });
+    }
+
+
+    return res.status(201).json({
+      status: 'success',
+      message: 'successfully updated'
+    });
+
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+
+  }
+
+
+}
+
+
+module.exports.productRemove = async (req, res) => {
+  const imagePath = req.query.imagePath;
+  const id = req.params.id;
+
+  try {
+    if (!imagePath) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'please provide imagePath'
+      });
+    } else {
+
+      fs.unlink(`.${imagePath}`, async (err) => {
+        if (err) {
+          return res.status(400).json({
+            status: 'error',
+            message: `${err}`
+          });
+        } else {
+          await Product.findByIdAndDelete({ _id: id });
+        }
+      });
+
+    }
+
+
+  } catch (err) {
+    return res.status(400).json({
+      status: 'error',
+      message: `${err}`
+    });
+  }
+
+
+}
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const mongoose = require('mongoose');
+const port = 5000;
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect('mongodb+srv://lucane:shubham@cluster0.jdevgjr.mongodb.net/Shops').then((result) => {
+  app.listen(port);
+}).catch((err) => {
+  console.log(err);
+})
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+  abortOnLimit: true,
+  createParentPath: true
+}));
+
+
+app.use(userRoutes);
+app.use(productRoutes);
+app.use(orderRoutes);
+
+
+
+
+
+
+
+
